7f4180efd90e125b90270fdf4d0bb57a5d789f73
bhargabdeka-deka <143352124+bhargabdeka-deka@users.noreply.github.com>
2026-02-26 01:42:18 +0530

    Debug Mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
-    console.log("URI:", process.env.MONGO_URI);
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
 
     await mongoose.connect(process.env.MONGO_URI);
 
