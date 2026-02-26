e479d1a205f83fc93b6db8199a1956c5b8e66cb3
bhargabdeka-deka <143352124+bhargabdeka-deka@users.noreply.github.com>
2026-02-27 00:20:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+//
+console.log("TEST_VAR:", process.env.TEST_VAR);
+console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+//
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
