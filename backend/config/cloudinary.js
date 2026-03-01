99df9c510d7b2d2632e98d93c452924745450242
bhargabdeka-deka <143352124+bhargabdeka-deka@users.noreply.github.com>
2026-03-02 01:44:36 +0530

    change the cloudinary file

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,4 +1,18 @@
+// const cloudinary = require("cloudinary").v2;
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// module.exports = cloudinary;
+///////////////////////////////////
 const cloudinary = require("cloudinary").v2;
+
+console.log("CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API KEY:", process.env.CLOUDINARY_API_KEY ? "Exists" : "Missing");
+console.log("API SECRET:", process.env.CLOUDINARY_API_SECRET ? "Exists" : "Missing");
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
